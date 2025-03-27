62fd6846a7150a128865c38d8def16f91c34ec9f
Jenish Patel <jpatel102005@gmail.com>
2025-03-27 16:35:34 -0400

    Update instagramscrap.py

--- instagramscrap.py
+++ instagramscrap.py
@@ -1,37 +1,37 @@
 from flask import Flask, request, jsonify
-2from instaloader import Instaloader
-3
-4app = Flask(__name__)
-5insta_loader = Instaloader()
-6
-7@app.route('/download_posts', methods=['GET'])
-8def download_posts():
-9    username = request.args.get('username')
-10    if not username:
-11        return jsonify({'error': 'Username is required'}), 400
-12
-13    try:
-14        profile = insta_loader.check_profile_id(username)
-15        posts = insta_loader.get_posts(profile)
-16        post_data = []
-17
-18        for post in posts:
-19            if len(post_data) >= 2:
-20                break
-21
-22            post_info = {
-23                'post_url': post.url,
-24                'post_caption': post.caption,
-25                'post_date': post.date_local,
-26                'post_likes': post.likes,
-27                'post_comments': post.comments
-28            }
-29            post_data.append(post_info)
-30
-31        return jsonify(post_data)
-32
-33    except Exception as e:
-34        return jsonify({'error': str(e)}), 500
-35
-36if __name__ == '__main__':
-37    app.run(debug=True)
+from instaloader import Instaloader
+
+app = Flask(__name__)
+insta_loader = Instaloader()
+
+@app.route('/download_posts', methods=['GET'])
+def download_posts():
+    username = request.args.get('username')
+    if not username:
+        return jsonify({'error': 'Username is required'}), 400
+
+    try:
+        profile = insta_loader.check_profile_id(username)
+        posts = insta_loader.get_posts(profile)
+        post_data = []
+
+        for post in posts:
+            if len(post_data) >= 2:
+                break
+
+            post_info = {
+                'post_url': post.url,
+                'post_caption': post.caption,
+                'post_date': post.date_local,
+                'post_likes': post.likes,
+                'post_comments': post.comments
+            }
+            post_data.append(post_info)
+
+        return jsonify(post_data)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
